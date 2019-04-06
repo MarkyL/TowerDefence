ab5ba8c7c9546129405a229d2e89eebea47fcbfd
Omri Ohayon <omri@rans-mbp-2>
2019-04-06 16:27:08 +0300

    First commit - project initialized

---
+++ MineSweeper/MineSweeper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MineSweeper
+//
+//  Created by Omri Ohayon on 06/04/2019.
+//  Copyright © 2019 Omri Ohayon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
