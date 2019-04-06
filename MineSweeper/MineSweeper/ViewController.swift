3bdd4a67eae96bd8f231259e29eab0c9d7527fa8
Mark Lurie <markl1@mail.afeka.ac.il>
2019-04-06 07:19:59 -0700

    Added user textfield + start of implementation of difficulty chooser (Won't be radio button but some kind of list)

--- MineSweeper/MineSweeper/ViewController.swift
+++ MineSweeper/MineSweeper/ViewController.swift
@@ -8,12 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var difficultyPickerView: UIPickerView!
+    
+    var difficultyList = [ "Easy", "Medium", "Hard" ]
+    
 
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return difficultyList.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        self.view.endEditing(true)
+        return difficultyList[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        //self.difficultyPickerView.isHidden = true
+    }
+    
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // mark test first commit
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
